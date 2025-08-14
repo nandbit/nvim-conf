f8a8b2886c0eb98199721b3f8c73c77130eec3e8
Galayda.Nikita 黎軒喬 IEC1 <galayda.nikita@inventec.com>
2025-08-14 18:19:04 +0800

    format

--- init.lua
+++ init.lua
@@ -5,11 +5,14 @@
 -- Then prepend it to runtime path
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,8 +25,8 @@ vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagn
 require("lazy").setup("plugins")
 
 -- 4. Basic settings
-vim.opt.number = true          -- line numbers
-vim.opt.relativenumber = true  -- relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
