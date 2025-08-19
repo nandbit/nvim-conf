a64867a0e17d8f695dae56edaa6f32aaf30ff24c
Galayda.Nikita 黎軒喬 IEC1 <galayda.nikita@inventec.com>
2025-08-19 15:19:21 +0800

    format

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,42 +1,49 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    require("mason").setup()
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"svelte",
+				"ts_ls",
+				"pyright",
+				"lua_ls",
+				"zls",
+				"html",
+				"cssls",
+			},
+		})
 
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "svelte",
-        "ts_ls",
-        "pyright",
-        "lua_ls",
-        "zls",
-        "html",
-        "cssls",
-      },
-    })
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local lspconfig = require("lspconfig")
+		-- Add nvim-cmp capabilities
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    lspconfig.lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    })
+		-- Lua LSP setup
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
 
-    local servers = { "svelte", "ts_ls", "pyright", "zls", "html", "cssls" }
-    for _, server in ipairs(servers) do
-      if server ~= "lua_ls" then
-        lspconfig[server].setup({})
-      end
-    end
-  end,
+		-- Other servers
+		local servers = { "svelte", "ts_ls", "pyright", "zls", "html", "cssls" }
+		for _, server in ipairs(servers) do
+			lspconfig[server].setup({
+				capabilities = capabilities,
+			})
+		end
+	end,
 }
-
